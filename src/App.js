3d22d30bec27d200feea288e9ead322184bd65da
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2024-05-11 07:55:27 +0200

    Added frontend to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route } from 'react-router-dom';
+import BodyWrapper from './components/BodyWrapper/BodyWrapper';
+import About from './components/about/About';
+import Categories from './components/categories/Categories';
+import Checkout from './components/checkout/Checkout';
+import Home from './components/home/Home';
+import Products from './components/products/Products';
+import Navbar from './components/navbar/NavBar';
+import Footer from './components/footer/Footer';
+import Cart from './components/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BodyWrapper />} />
+        <Route path="/about" element={<About/>}/>
+        <Route path="/categories" element={<Categories/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/cart" component={Cart} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
