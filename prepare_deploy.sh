d0cb51e16cf87cc04e14ddd4be3a92ccc1ae8957
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2024-05-11 12:22:41 +0200

    Updated prepare_deploy.sh

--- prepare_deploy.sh
+++ prepare_deploy.sh
@@ -9,7 +9,26 @@ TARGET_DIR="."
 
 # Copy the relevant files to the root directory
 # Exclude the directories and files that are not needed
-rsync -av --exclude='node_modules/' --exclude='.git/' --exclude='.gitattributes' --exclude='.gitignore' --exclude='build/' --exclude='lib/' --exclude='node/' --exclude='pom.xml' --exclude='readme.md' "$SRC_DIR/" "$TARGET_DIR"
+# Create directories if they do not exist
+mkdir -p "$TARGET_DIR/node_modules"
+mkdir -p "$TARGET_DIR/.git"
+mkdir -p "$TARGET_DIR/build"
+mkdir -p "$TARGET_DIR/lib"
+mkdir -p "$TARGET_DIR/node"
+
+# Use cp to copy files over
+cp -r "$SRC_DIR/"* "$TARGET_DIR"
+
+# Remove the directories and files that are not needed
+rm -rf "$TARGET_DIR/node_modules"
+rm -rf "$TARGET_DIR/.git"
+rm -rf "$TARGET_DIR/.gitattributes"
+rm -rf "$TARGET_DIR/.gitignore"
+rm -rf "$TARGET_DIR/build"
+rm -rf "$TARGET_DIR/lib"
+rm -rf "$TARGET_DIR/node"
+rm -f "$TARGET_DIR/pom.xml"
+rm -f "$TARGET_DIR/readme.md"
 
 # Add all changes to git
 git add -A
