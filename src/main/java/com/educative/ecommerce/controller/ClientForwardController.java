4517001da9c5cfa033b3aadd9ade0d546ed07002
tjrapodile <47654804+tjrapodile@users.noreply.github.com>
2024-05-15 23:49:18 +0200

    Fixed cross origigns error

--- src/main/java/com/educative/ecommerce/controller/ClientForwardController.java
+++ src/main/java/com/educative/ecommerce/controller/ClientForwardController.java
@@ -1,9 +1,11 @@
 package com.educative.ecommerce.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:5173")
 public class ClientForwardController {
     @GetMapping(value= "/**/{path:[^\\.]*}")
     public String forward(){
--- src/main/java/com/educative/ecommerce/controller/UserController.java
+++ src/main/java/com/educative/ecommerce/controller/UserController.java
@@ -6,13 +6,11 @@ import com.educative.ecommerce.dto.user.SignInReponseDto;
 import com.educative.ecommerce.dto.user.SignupDto;
 import com.educative.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("user")
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     @Autowired
